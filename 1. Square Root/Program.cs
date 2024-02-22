7dde7a6af2ba53456d35654bf5a9370bbcef78a8
s-toshev <simeon.toshev@yahoo.bg>
2024-02-22 23:23:25 +0200

    22 Feb

---
+++ 1. Square Root/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _1._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if(num < 0)
+                {
+                    Console.WriteLine("Invalid number.");
+                }
+                else{
+                    double sqRoot = Math.Sqrt(num);
+                    Console.WriteLine(sqRoot);
+
+                }
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number.");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+
+            }
+
+
+        }
+    }
+}
