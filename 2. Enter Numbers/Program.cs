09882f8d17a73bad19942636ecb3c2dec3d86985
s-toshev <simeon.toshev@yahoo.bg>
2024-02-23 09:31:50 +0200

    23 Feb

--- 2. Enter Numbers/Program.cs
+++ 2. Enter Numbers/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace _2._Enter_Numbers
+﻿using System.Text;
+
+namespace _2._Enter_Numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<int> ints = new List<int>();
+            int prevNum = 1;
+
+            while (ints.Count < 10)
+            {
+                try
+                {
+                    int num = ReadNumber(prevNum, 100);
+
+                    ints.Add(num);
+                    prevNum = num;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine($"Your number is not in range " +
+                        $"{prevNum} - 100!");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(String.Join(", ", ints));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentException();
+            }
+
+            return num;
         }
     }
 }
---
+++ 3. Sum of Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace _3._Sum_of_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split(" ").ToList();
+
+            int sum = 0;
+            foreach (var item in list)
+            {
+                try
+                {
+
+                    int element = int.Parse(item);
+                    sum += element;
+
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+
+                }
+
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+
+        }
+    }
+}
