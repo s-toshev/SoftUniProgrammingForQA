76b293f5bcebf521885032c9b4277ddd9e14e455
s-toshev <simeon.toshev@yahoo.bg>
2024-02-23 21:29:02 +0200

    23 Feb

---
+++ OOP-Principles-Encapsulation-Inheritance-/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP_Principles_Encapsulation_Inheritance_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+
+            Console.WriteLine("These exercises will be added as another repo in my github account: s-toshev");
+        }
+    }
+}
