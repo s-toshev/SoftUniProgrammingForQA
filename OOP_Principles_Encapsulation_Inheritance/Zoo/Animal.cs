49acf49f9661c920e2704c49ebd3f2128bbc3065
s-toshev <simeon.toshev@yahoo.bg>
2024-02-24 15:49:34 +0200

    24 Feb

--- OOP_Principles_Encapsulation_Inheritance/Zoo/Animal.cs
+++ OOP_Principles_Encapsulation_Inheritance/Zoo/Animal.cs
@@ -2,5 +2,17 @@
 
 public class Animal
 {
+    private string _name;
+
+    public string Name
+    {
+        get { return _name; }
+        private set { _name = value; }
+    }
+
+    public Animal(string name)
+    {
+        this.Name = name;
+    }
 
 }
