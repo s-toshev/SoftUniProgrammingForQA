41861c4cb48f964998ed20309b8136f7544bce5d
s-toshev <simeon.toshev@yahoo.bg>
2024-02-25 02:41:39 +0200

    25 Feb

---
+++ OOP-Principles-Abstraction-Polymorphism/Raiding/Models/Heroes/Rogue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Models.Heroes
+{
+    public class Rogue : Fighter
+    {
+        public Rogue(string name) : base(name)
+        {
+        }
+
+        public override int Power => 80;
+
+        public override string CastAbility()
+        {
+            return base.CastAbility();
+        }
+    }
+
+}
